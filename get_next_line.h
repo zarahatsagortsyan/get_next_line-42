9823c439932a325ad9786fa47d71bbfc8144d171
Zaruhi Hatsagortsyan <zhatsago@c2r5s2.42yerevan.am>
2022-08-06 15:58:07 +0400

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhatsago <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 16:48:24 by zhatsago          #+#    #+#             */
+/*   Updated: 2021/03/02 17:45:08 by zhatsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *str);
+char	*the_line(char *tmp);
+char	*leftover(char *tmp);
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhatsago <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 16:47:47 by zhatsago          #+#    #+#             */
+/*   Updated: 2021/03/02 17:45:13 by zhatsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	if (!(join = malloc(sizeof(char) *
+					(ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+		while (s1[i] != '\0')
+			join[j++] = s1[i++];
+	i = 0;
+	if (s2)
+		while (s2[i] != '\0')
+			join[j++] = s2[i++];
+	join[j] = '\0';
+	free(s1);
+	return (join);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (c == '\0')
+		return (&((char *)s)[ft_strlen(s)]);
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return (NULL);
+}
